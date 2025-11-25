aa7c959b8be1c22e0b452c952050356ec61fcc9c
Katie <katiemkov@gmail.com>
2025-11-24 23:15:36 -0800

    refactor

--- exercises/lab08.js
+++ exercises/lab08.js
@@ -1,25 +1,31 @@
-$("#creature").click(function() {
-  $("#status").text("You clicked me! 🐾");
-  $("#creature").css("background", "lavender");
+var $creature = $("#creature");
+var $status = $("#status");
+
+function setStatus(msg) {
+  $status.text(msg);
+}
+
+$creature.on("click", function () {
+  setStatus("You clicked me! 🐾");
+  $creature.css("background", "lavender");
 });
 
-
-$("#creature").hover(
-  function() { $("#status").text("You’re close... 👀"); },
-  function() { $("#status").text("You left me :("); }
+$creature.hover(
+  function () { setStatus("You’re close... 👀"); },
+  function () { setStatus("You left me :("); }
 );
 
-$("#creature").dblclick(function() {
-  $("#status").text("You woke me up!! 😳");
-  $("#creature").css("transform", "scale(1.2)");
+$creature.on("dblclick", function () {
+  setStatus("You woke me up!! 😳");
+  $creature.css("transform", "scale(1.2)");
 });
 
-$(document).keydown(function(event) {
-  $("#status").text("You pressed: " + event.key);
+$(document).on("keydown", function (event) {
+  setStatus("You pressed: " + event.key);
 });
 
-$(document).mousemove(function(event) {
-  $("#creature").css({
+$(document).on("mousemove", function (event) {
+  $creature.css({
     left: event.pageX - 60,
     top: event.pageY - 60,
     position: "absolute"
